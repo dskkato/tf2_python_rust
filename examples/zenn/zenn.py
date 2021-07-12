0fa6911bd1d15901181a3728ddca1ab6480b687a
dskkato <kato.daisuke429@gmail.com>
2021-07-13 08:07:09 +0900

    add name

--- examples/zenn/zenn.py
+++ examples/zenn/zenn.py
@@ -7,7 +7,7 @@ from numpy.testing import assert_almost_equal
 # default input shape 224x224x3
 model = tf.keras.applications.MobileNetV3Large()
 
-x = tf.TensorSpec(model.input_shape, tf.float32)
+x = tf.TensorSpec(model.input_shape, tf.float32, name="x")
 concrete_function = tf.function(lambda x: model(x)).get_concrete_function(x)
 # now all variables are converted to constants.
 # if this step is omitted, dumped graph does not include trained weights
@@ -23,6 +23,7 @@ original_pred = model(sample)
 
 frozen_model_pred = frozen_model(tf.cast(sample, tf.float32))[0]
 assert_almost_equal(original_pred.numpy(), frozen_model_pred.numpy())
-decoded = tf.keras.applications.mobilenet_v3.decode_predictions(original_pred.numpy())[0]
+decoded = tf.keras.applications.mobilenet_v3.decode_predictions(original_pred.numpy())[
+    0
+]
 print(decoded)
-
